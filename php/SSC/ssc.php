7ddf2fe89aa22bb5ef780a3a2dc62d5fbfcd27f8
AbstractDil <nandysagar@yahoo.com>
2023-08-19 17:06:25 +0530

    updated

--- php/SSC/ssc.php
+++ php/SSC/ssc.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        SSC CGL &amp; CHSL Score Calculator
+        MathHub Score Card Generating System v1.0.0
     </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
